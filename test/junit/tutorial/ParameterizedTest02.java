90c42eafb37d743a2a8c5063965a1e7a87924fb2
kimura <pankata79.th@gmail.com>
2020-10-30 15:22:56 +0900

    DataPointsを使用

--- test/junit/tutorial/ParameterizedTest02.java
+++ test/junit/tutorial/ParameterizedTest02.java
@@ -4,15 +4,24 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.DataPoints;
 import org.junit.experimental.theories.Theories;
 import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 @RunWith(Theories.class)
 public class ParameterizedTest02 {
+	// DataPointsを使用した定義
+	@DataPoints
+	public static Fixture[] INT_PARAMS = {
+			new Fixture(1,2,3),
+			new Fixture(0,2,2),
+	};
+	// DataPointを使用した定義
 	@DataPoint
 	public static Fixture INT_PARAM_1 =new Fixture(1,2,3);
 	@DataPoint
 	public static Fixture INT_PARAM_2 = new Fixture(0,2,2);
+	
 	
 	@SuppressWarnings("deprecation")
 	@Theory
