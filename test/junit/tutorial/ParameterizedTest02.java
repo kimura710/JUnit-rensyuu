c04b60119f78e02eadad7aa04418c6bc1f95a583
kimura <pankata79.th@gmail.com>
2020-10-30 15:19:39 +0900

    フィクスチャオブジェクトの利用

---
+++ test/junit/tutorial/ParameterizedTest02.java
@@ -0,0 +1,35 @@
+package junit.tutorial;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+@RunWith(Theories.class)
+public class ParameterizedTest02 {
+	@DataPoint
+	public static Fixture INT_PARAM_1 =new Fixture(1,2,3);
+	@DataPoint
+	public static Fixture INT_PARAM_2 = new Fixture(0,2,2);
+	
+	@SuppressWarnings("deprecation")
+	@Theory
+	public void testCase(Fixture params)throws Exception{
+		assertThat(params.x + params.y, is(params.expected));
+	}
+
+	
+}
+class Fixture{
+	int x;
+	int y;
+	int expected;
+	
+	Fixture(int x,int y,int expected){
+		this.x = x;
+        this.y = y;
+        this.expected = expected;
+	}
+}
